c5b907a89cc5504ecf21849fa68da46c1f773454
Priyal Khandelwal <priyalkhandelwal28@gmail.com>
2026-05-11 17:05:51 +0530

    Fix cookie settings for production

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -13,8 +13,8 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "strict", // CSRF attacks
-    secure: false
+    sameSite: "none", // CSRF attacks
+    secure: true
   });
 
   return token;
