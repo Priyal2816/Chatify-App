3f0cc451b97a8e6b43b8da611b6c37cd1e2f9816
Priyal Khandelwal <priyalkhandelwal28@gmail.com>
2026-05-11 14:37:17 +0530

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,24 @@
+import "dotenv/config";
+
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
+import { app, server } from "./lib/socket.js";
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json({ limit: "5mb" })); // req.body
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  connectDB();
+});
