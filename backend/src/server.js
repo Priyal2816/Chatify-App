3704225672cdc67e960051bd7d03c8829f4ea410
Priyal Khandelwal <priyalkhandelwal28@gmail.com>
2026-05-11 17:20:08 +0530

    Fix CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,12 @@ import { app, server } from "./lib/socket.js";
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json({ limit: "5mb" })); // req.body
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ 
+  origin: [
+     process.env.CLIENT_URL, 
+     "https://chatify-app-eta.vercel.app",
+      "http://localhost:5173"
+  ], credentials: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
